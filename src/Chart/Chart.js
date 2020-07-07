eb26ed17571d117386f7ca24a866cd7a2e497853
Tess Gronski <50762504+tcgronk@users.noreply.github.com>
2020-07-06 21:13:45 -0700

    Adds lines to covid chart

--- src/Chart/Chart.js
+++ src/Chart/Chart.js
@@ -1,14 +1,12 @@
 
 import React, { Component } from "react";
 import * as d3 from "d3";
-import covid from "../covid.js"
+import cases from "../covid.js"
 
 export default class Chart extends Component {
 
   render(){
-    const dataset=[{"positive":1712637,"dateChecked":"2020-05-28T20:00:00Z"},{"positive":1689630,"dateChecked":"2020-05-27T20:00:00Z"},{"positive":1671035, "dateChecked":"2020-05-26T20:00:00Z"}]
-
-   const data=dataset
+   const dataset=cases
     
     const yAccessor = d => d.positive
     const dateParser = d3.utcParse("%Y-%m-%dT%H:%M:%S%Z")
@@ -95,7 +93,6 @@ export default class Chart extends Component {
     //     .style("transform", `translateY(${
     //       dimensions.boundedHeight
     //     }px)`)
-    console.log(lineGenerator(dataset))
 
   const ylabel="Number of Cases"
   const xlabel="Date"
@@ -106,6 +103,10 @@ export default class Chart extends Component {
       <g transform={`translate(0, ${dimensions.boundedHeight})`}><line
         className="Axis__x__line" 
         x2={dimensions.boundedWidth}
+        stroke={"#000000"} strokeWidth={2}
+        style={{
+          transform: `translate(30px, 0)`
+        }}
       />
       <text transform={`translate(${dimensions.boundedWidth / 2},40)`}>{xlabel}</text>
    
@@ -114,18 +115,25 @@ export default class Chart extends Component {
       <g ><line 
         className="Axis__y__line"
         y2={dimensions.boundedHeight}
-       
+        stroke={"#000000"} strokeWidth={2}
+        style={{
+          transform: `translate(30px, 0)`
+        }}
       />
       <text style={{
             transform: `translate(20px, ${dimensions.boundedHeight / 1.5}px) rotate(-90deg)`
           }}> {ylabel}</text>
       </g>
 
-
+{/* 
           <g style={{transform:`translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`}}>
           <rect x={0} y={numberOfCases} width={dimensions.boundedWidth} height={dimensions.boundedHeight} fill= {"#eeee"}/>
-          </g>
-          <path  data={data} d={lineGenerator(data)}  fill={"none"} stroke={"#E6E831"} strokeWidth={2}/>
+          </g> */}
+          <path  data={dataset} d={lineGenerator(dataset)}  fill={"none"} stroke={"#E6E831"} strokeWidth={2}
+                  style={{
+                    transform: `translate(30px, 0)`
+                  }}
+          />
         </svg>
    </div>
   );
