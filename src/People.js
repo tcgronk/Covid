b9e2b8e08c199aebb423a02510730fc9d9467a60
Tess Gronski <50762504+tcgronk@users.noreply.github.com>
2020-07-12 16:11:33 -0700

    Updates font size on people route

--- src/People.js
+++ src/People.js
@@ -10,21 +10,21 @@ export default class People extends Component {
     <div>
       <h2>How to help your community:</h2>
 
-      <h2>Volunteering</h2>
-      <h3>Start a 'virtual' blood drive</h3>
-      <h3>Make masks</h3>
-      <h3>Start a food drive</h3>
+      <h3>Volunteering</h3>
+      <h4>Start a 'virtual' blood drive</h4>
+      <h4>Make masks</h4>
+      <h4>Start a food drive</h4>
 
-      <h2>Lifestyle Changes</h2>
-      <h3>Wear a mask when outside</h3>
-      <h3>Check in on family & neighbors to see if they need help with groceries or errands</h3>
-      <h3>Stay at home or move social events to video meetings</h3>
-      <h3>Order take-out for your local hospital staff</h3>
+      <h3>Lifestyle Changes</h3>
+      <h4>Wear a mask when outside</h4>
+      <h4>Check in on family & neighbors to see if they need help with groceries or errands</h4>
+      <h4>Stay at home or move social events to video meetings</h4>
+      <h4>Order take-out for your local hospital staff</h4>
 
-      <h2>Donate</h2>
-      <h3>Donate toilet paper & feminine hygiene products to local shelters</h3>
-      <h3>Donate money you would have spent on gas or public transit</h3>
-      <h3>Buy giftcards to restaurants, salons, and other impacted businesses</h3>
+      <h3>Donate</h3>
+      <h4>Donate toilet paper & feminine hygiene products to local shelters</h4>
+      <h4>Donate money you would have spent on gas or public transit</h4>
+      <h4>Buy giftcards to restaurants, salons, and other impacted businesses</h4>
    </div>
   );
   }
