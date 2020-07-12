e25acb91dfb7009e0e1ae9fe1a8e723fa34d8b1d
Tess Gronski <50762504+tcgronk@users.noreply.github.com>
2020-07-11 22:04:14 -0700

    Adds notes for community route

--- src/People.js
+++ src/People.js
@@ -8,7 +8,23 @@ export default class People extends Component {
 
   return (
     <div>
+      <h2>How to help your community:</h2>
 
+      <h2>Volunteering</h2>
+      <h3>Start a 'virtual' blood drive</h3>
+      <h3>Make masks</h3>
+      <h3>Start a food drive</h3>
+
+      <h2>Lifestyle Changes</h2>
+      <h3>Wear a mask when outside</h3>
+      <h3>Check in on family & neighbors to see if they need help with groceries or errands</h3>
+      <h3>Stay at home or move social events to video meetings</h3>
+      <h3>Order take-out for your local hospital staff</h3>
+
+      <h2>Donate</h2>
+      <h3>Donate toilet paper & feminine hygiene products to local shelters</h3>
+      <h3>Donate money you would have spent on gas or public transit</h3>
+      <h3>Buy giftcards to restaurants, salons, and other impacted businesses</h3>
    </div>
   );
   }
