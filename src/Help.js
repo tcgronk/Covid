a3e1d67a2a7750cae34c2716137938fba77d55c4
Tess Gronski <tessgronski@gmail.com>
2020-07-20 22:40:34 -0700

    Updates spacing on how to help list

--- src/Help.js
+++ src/Help.js
@@ -12,13 +12,13 @@ export default class Help extends Component {
        <p>Protect yourself & your community through education and information sharing:</p>
          <div className="list">
          <h2><a href='https://www.unwomen.org/en' alt='un-information' target='_blank'>
-  {" "}1 UN Women
+  {" "}1. UN Women
             </a> </h2> 
            <h2><a href='https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public'alt='who-information' target='_blank'>
-  {" "}2 WHO
+  {" "}2. WHO
             </a> </h2> 
             <h2><a href='https://www.cdc.gov/coronavirus/'alt='cdc-information' target='_blank'>
-  {" "}3 CDC
+  {" "}3. CDC
             </a> </h2> 
 
             </div>
